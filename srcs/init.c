c91e528212a53d8d24b1f8422fd85c482717ff35
Alix <155173056+Alibababs@users.noreply.github.com>
2024-07-29 17:20:37 +0200

    Add files via upload

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: pbailly <pbailly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 18:28:35 by alibabab          #+#    #+#             */
-/*   Updated: 2024/07/29 16:22:56 by pbailly          ###   ########.fr       */
+/*   Updated: 2024/07/29 17:16:36 by pbailly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	init_background(t_data *data)
 	data->background = mlx_xpm_file_to_image(data->mlx, path_img, &img_width,
 			&img_height);
 	if (!data->background)
-		error_msg("Error\n", data);
+		error_msg("Error\nImage of the background couldn't load", data);
 	y = 0;
 	while (y < data->size_y)
 	{
@@ -60,7 +60,7 @@ void	init_obj(t_data *data, char *path)
 		mlx_destroy_image(data->mlx, data->obj);
 	data->obj = mlx_xpm_file_to_image(data->mlx, path, &img_width, &img_height);
 	if (!data->obj)
-		error_msg("Error\n", data);
+		error_msg("Error\nImage of the map couldn't load", data);
 	mlx_put_image_to_window(data->mlx, data->win, data->obj, (data->x * IMG_W),
 		(data->y * IMG_H));
 }
@@ -76,8 +76,8 @@ void	init_player(t_data *data, char *path)
 		mlx_destroy_image(data->mlx, data->player);
 	data->player = mlx_xpm_file_to_image(data->mlx, path, &img_width,
 			&img_height);
-	if (!data->text_box)
-		error_msg("Error\n", data);
+	if (!data->player)
+		error_msg("Error\nImage of the player couldn't load", data);
 	mlx_put_image_to_window(data->mlx, data->win, data->player, (data->x
 			* IMG_W), (data->y * IMG_H));
 }
@@ -90,7 +90,7 @@ void	init_text_box(t_data *data)
 	data->text_box = mlx_xpm_file_to_image(data->mlx, "./textures/text_box.xpm",
 			&img_width, &img_height);
 	if (!data->text_box)
-		error_msg("Error\n", data);
+		error_msg("Error\nImage of the text_box couldn't load", data);
 	mlx_put_image_to_window(data->mlx, data->win, data->text_box, 0, 0);
 	mlx_string_put(data->mlx, data->win, 24, 25, 0xDCDCDC, "Movements:");
 	mlx_string_put(data->mlx, data->win, 90, 26, 0xDCDCDC, "0");
