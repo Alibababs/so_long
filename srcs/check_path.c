e62892dc693aa1d6d3f89102b5b8ee6effa692d1
Alibababs <alibaba@Alix>
2024-07-29 20:26:39 +0200

    FIX

--- srcs/check_path.c
+++ srcs/check_path.c
@@ -3,37 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   check_path.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pbailly <pbailly@student.42.fr>            +#+  +:+       +#+        */
+/*   By: alibaba <alibaba@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 10:59:02 by pbailly           #+#    #+#             */
-/*   Updated: 2024/07/28 19:31:29 by pbailly          ###   ########.fr       */
+/*   Updated: 2024/07/29 19:56:09 by alibaba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-static void	find_player_position(t_data *data, t_point *player_pos)
-{
-	int	y;
-	int	x;
-
-	y = 0;
-	while (y < data->size_y / IMG_H)
-	{
-		x = 0;
-		while (x < data->size_x / IMG_W)
-		{
-			if (data->map[y][x] == 'P')
-			{
-				player_pos->x = x;
-				player_pos->y = y;
-				return ;
-			}
-			x++;
-		}
-		y++;
-	}
-}
 
 static int	is_valid_move(t_data *data, int x, int y, int **visited)
 {
@@ -42,22 +19,24 @@ static int	is_valid_move(t_data *data, int x, int y, int **visited)
 		&& !visited[y][x]);
 }
 
-static void	explore_map(t_data *data, int x, int y, int **visited)
+static void	explore_map(t_data *data, int x, int y, int **visited, int consider_exit)
 {
 	if (!is_valid_move(data, x, y, visited))
+		return ;
+	if (data->map[y][x] == 'E' && !consider_exit)
 		return ;
 	visited[y][x] = 1;
 	if (data->map[y][x] == 'C')
 		data->c_acces++;
-	if (data->map[y][x] == 'E')
+	if (data->map[y][x] == 'E' && consider_exit)
 		data->exit_found = 1;
-	explore_map(data, x + 1, y, visited);
-	explore_map(data, x - 1, y, visited);
-	explore_map(data, x, y + 1, visited);
-	explore_map(data, x, y - 1, visited);
+	explore_map(data, x + 1, y, visited, consider_exit);
+	explore_map(data, x - 1, y, visited, consider_exit);
+	explore_map(data, x, y + 1, visited, consider_exit);
+	explore_map(data, x, y - 1, visited, consider_exit);
 }
 
-static void	init_visited(t_data *data, int **visited)
+static void	init_visited(t_data *data, int **visited, int init)
 {
 	int	i;
 	int	j;
@@ -65,7 +44,12 @@ static void	init_visited(t_data *data, int **visited)
 	i = 0;
 	while (i < data->size_y / IMG_H)
 	{
-		visited[i] = malloc((data->size_x / IMG_W) * sizeof(int));
+		if (init)
+		{
+			visited[i] = malloc((data->size_x / IMG_W) * sizeof(int));
+			if (!visited[i])
+				error_msg("Error\nUnable to allocate memory for visited.\n", data);
+		}
 		j = 0;
 		while (j < data->size_x / IMG_W)
 		{
@@ -76,18 +60,42 @@ static void	init_visited(t_data *data, int **visited)
 	}
 }
 
+static void check_collectibles(t_data *data, int **visited)
+{
+	int y;
+	int x;
+
+	y = 0;
+	while (y < data->size_y / IMG_H)
+	{
+		x = 0;
+		while (x < data->size_x / IMG_W)
+		{
+			if (data->map[y][x] == 'C')
+			{
+				data->c_acces = 0;
+				init_visited(data, visited, 0);
+				explore_map(data, x, y, visited, 0);
+				if (data->c_acces == 0)
+					error_msg("Error\nNo valid path to a collectible\n", data);
+			}
+			x++;
+		}
+		y++;
+	}
+}
+
 void	path_checker(t_data *data)
 {
-	t_point	player_pos;
 	int		**visited;
 	int		i;
 
-	find_player_position(data, &player_pos);
 	visited = malloc((data->size_y / IMG_H) * sizeof(int *));
 	if (!visited)
 		error_msg("Error\nUnable to allocate memory for visited.\n", data);
-	init_visited(data, visited);
-	explore_map(data, player_pos.x, player_pos.y, visited);
+	init_visited(data, visited, 1);
+	explore_map(data, data->p_x, data->p_y, visited, 1);
+	check_collectibles(data, visited);
 	i = 0;
 	while (i < data->size_y / IMG_H)
 		free(visited[i++]);
--- srcs/init.c
+++ srcs/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pbailly <pbailly@student.42.fr>            +#+  +:+       +#+        */
+/*   By: alibaba <alibaba@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 18:28:35 by alibabab          #+#    #+#             */
-/*   Updated: 2024/07/29 17:16:36 by pbailly          ###   ########.fr       */
+/*   Updated: 2024/07/29 20:12:54 by alibaba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	init_background(t_data *data)
 	data->background = mlx_xpm_file_to_image(data->mlx, path_img, &img_width,
 			&img_height);
 	if (!data->background)
-		error_msg("Error\nImage of the background couldn't load", data);
+		error_msg("Error\nBackground image failed to load.", data);
 	y = 0;
 	while (y < data->size_y)
 	{
@@ -60,7 +60,7 @@ void	init_obj(t_data *data, char *path)
 		mlx_destroy_image(data->mlx, data->obj);
 	data->obj = mlx_xpm_file_to_image(data->mlx, path, &img_width, &img_height);
 	if (!data->obj)
-		error_msg("Error\nImage of the map couldn't load", data);
+		error_msg("Error\nImage failed to load.", data);
 	mlx_put_image_to_window(data->mlx, data->win, data->obj, (data->x * IMG_W),
 		(data->y * IMG_H));
 }
@@ -77,7 +77,7 @@ void	init_player(t_data *data, char *path)
 	data->player = mlx_xpm_file_to_image(data->mlx, path, &img_width,
 			&img_height);
 	if (!data->player)
-		error_msg("Error\nImage of the player couldn't load", data);
+		error_msg("Error\nPlayer image failed to load.", data);
 	mlx_put_image_to_window(data->mlx, data->win, data->player, (data->x
 			* IMG_W), (data->y * IMG_H));
 }
@@ -90,7 +90,7 @@ void	init_text_box(t_data *data)
 	data->text_box = mlx_xpm_file_to_image(data->mlx, "./textures/text_box.xpm",
 			&img_width, &img_height);
 	if (!data->text_box)
-		error_msg("Error\nImage of the text_box couldn't load", data);
+		error_msg("Error\nText box image failed to load.", data);
 	mlx_put_image_to_window(data->mlx, data->win, data->text_box, 0, 0);
 	mlx_string_put(data->mlx, data->win, 24, 25, 0xDCDCDC, "Movements:");
 	mlx_string_put(data->mlx, data->win, 90, 26, 0xDCDCDC, "0");
